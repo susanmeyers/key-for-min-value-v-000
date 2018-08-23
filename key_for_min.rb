9a0c519d11e91f3ea8655b1c129392e9d0091a30
Susan Meyers <smmeyers24@gmail.com>
2018-08-23 21:25:27 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,5 +2,12 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
+  #hash = {:blake => 500, :ashley => 2, :adam => 1}
+  
+    hash.sort_by { |key, value| value }.first
+    
+      
+    
+
 
 end
